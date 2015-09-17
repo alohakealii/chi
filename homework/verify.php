929a92a9136b9c8c779dac5fa643f07ed04654f9
quibs <quibie7@gmail.com>
2015-09-17 15:19:47 -0700

    nav bar added
    ugly nav bar added

--- homework/verify.php
+++ homework/verify.php
@@ -1,84 +1,104 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Chi</title>
+    <link rel="stylesheet" href="chi.css">
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+            <li><a href="#">Link</a></li>
+        </ul>
+    </nav>
+   
+    <?php
 
-try {
-    $con = new PDO("mysql:host=localhost;dbname=chi", "root", "root");
-    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    // declare variables from form
-    $first = $_POST['first'];
-    $last = $_POST['last'];
-    $age = $_POST['age'];
-    $gender = $_POST['gender'];
-    if (isset($_POST['ageControl'])) { $ageControl = $_POST['ageControl']; }
+    try {
+        $con = new PDO("mysql:host=localhost;dbname=chi", "root", "root");
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+        // declare variables from form
+        $first = $_POST['first'];
+        $last = $_POST['last'];
+        $age = $_POST['age'];
+        $gender = $_POST['gender'];
+        if (isset($_POST['ageControl'])) { $ageControl = $_POST['ageControl']; }
 
-    // build query based on selected parameters
-    $parameterCount = 0;
-    $query = "SELECT * FROM profiles WHERE";
-    if ((strlen($first) > 0)) {
-        $query .= " first='$first'";
-        $parameterCount++;
-    }
-
-    if ((strlen($last) > 0)) {
-        if ($parameterCount > 0) { $query .= " AND"; }
-        $query .= " last='$last'";
-        $parameterCount++;
-    }
-
-    if ((strlen($age) > 0) && (isset($ageControl))) {
-        if ($parameterCount > 0) { $query .= " AND"; }
-        $query .= " age $ageControl $age";
-        $parameterCount++;
-    }
-
-    if ((strlen($gender) > 0)) {
-        if ($parameterCount > 0) { $query .= " AND"; }
-        if ($gender == "both") {
-            $query .= " (gender='male' or gender='female')"; }
-        else { 
-            $query .= " gender='$gender'";
+        // build query based on selected parameters
+        $parameterCount = 0;
+        $query = "SELECT * FROM profiles WHERE";
+        if ((strlen($first) > 0)) {
+            $query .= " first='$first'";
             $parameterCount++;
         }
-        
-    }
 
-    // if no parameters selected, return all
-    if ($parameterCount == 0) { $query = "SELECT * FROM profiles"; }
+        if ((strlen($last) > 0)) {
+            if ($parameterCount > 0) { $query .= " AND"; }
+            $query .= " last='$last'";
+            $parameterCount++;
+        }
 
-    // for debugging purposes
-    // echo $query;
+        if ((strlen($age) > 0) && (isset($ageControl))) {
+            if ($parameterCount > 0) { $query .= " AND"; }
+            $query .= " age $ageControl $age";
+            $parameterCount++;
+        }
 
-    $data = $con -> query($query);
-    $data -> setFetchMode(PDO::FETCH_ASSOC);
-    $rowCount = $data -> rowCount();
+        if ((strlen($gender) > 0)) {
+            if ($parameterCount > 0) { $query .= " AND"; }
+            if ($gender == "both") {
+                $query .= " (gender='male' or gender='female')"; }
+            else { 
+                $query .= " gender='$gender'";
+                $parameterCount++;
+            }
+            
+        }
 
-    $doHeader = true;    
-    if ($rowCount == 0) {
-        echo "no matching results";
-    } else {
-        $table = "<table border='1'><tr>";
-        // for loop to create headers
-        foreach ($data as $row) {
-            if ($doHeader) {
-                foreach ($row as $header => $value) {
-                    $table .= "<th>$header</th>";
+        // if no parameters selected, return all
+        if ($parameterCount == 0) { $query = "SELECT * FROM profiles"; }
+
+        // for debugging purposes
+        // echo $query;
+
+        $data = $con -> query($query);
+        $data -> setFetchMode(PDO::FETCH_ASSOC);
+        $rowCount = $data -> rowCount();
+
+        $doHeader = true;    
+        if ($rowCount == 0) {
+            echo "no matching results";
+        } else {
+            $table = "<table border='1'><tr>";
+            // for loop to create headers
+            foreach ($data as $row) {
+                if ($doHeader) {
+                    foreach ($row as $header => $value) {
+                        $table .= "<th>$header</th>";
+                    }
+                    $table .= "</tr>";
+                    $doHeader = false;
+                }
+
+                // for loop to populate rows with results
+                foreach ($row as $name => $value) {
+                    $table .= "<td>$value</td>";
                 }
                 $table .= "</tr>";
-                $doHeader = false;
             }
-
-            // for loop to populate rows with results
-            foreach ($row as $name => $value) {
-                $table .= "<td>$value</td>";
-            }
-            $table .= "</tr>";
+            $table .= "</table>";
+        
+            echo $table;
         }
-        $table .= "</table>";
-    
-        echo $table;
+    } catch (PDOException $ex) {
+        echo 'ERROR: '.$ex->getMessage();
     }
-} catch (PDOException $ex) {
-	echo 'ERROR: '.$ex->getMessage();
-}
 
-?>
+    ?>
+
+
+    </body>
+</html>
