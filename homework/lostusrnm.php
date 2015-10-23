7772ccb40dc38a8383334241b4c7f5044f4a5992
smlltrckr <smlltrckr@gmail.com>
2015-10-22 20:24:21 -0700

    Changed assignment 3 php to use ORM

--- homework/lostusrnm.php
+++ homework/lostusrnm.php
@@ -30,66 +30,135 @@
 
             <main>
             	<?php
-                  function constructTable($data){
-                    print("<table border='1'>\n");
+                class Lostname
+                {
+                    private $username;
 
-                    // Constructs table header
-                    $doHeader = true;
-                    foreach ($data as $row) {
-                        if($doHeader){
-                            print("<tr>");
-                            foreach ($row as $name => $value) {
-                                print("<th>$name</th>\n");
-                            }
-                            print("</tr>\n");
-                            $doHeader = false;
-                        }
-                        // Constructs table row
-                        print("<tr>\n");
-                        foreach ($row as $name => $value) {
-                            print("<td>$value</td>\n");
-                        }
-                        print("</tr>\n"); 
-                    }
-                    print("</table>\n");
+                    public function getUsername() {return $this->username;}
+                }
+
+                function createTableRow(Lostname $l){
+                    print "        <tr>\n";
+                    print "            <td>" . $l->getUsername()     . "</td>\n";
+                    print "        </tr>\n";
                 }
 
                 $firstName = filter_input(INPUT_POST, "firstName");
-                $lastName = filter_input(INPUT_POST, "lastName");
+                $lastName  = filter_input(INPUT_POST, "lastName");
 
                 try{
-                	if(empty($firstName) || empty($lastName)){
-                		throw new Exception("Missing first or last name");
-                	}
+                    $con = new PDO("mysql:host=localhost;dbname=chi", "root", "root");
+                    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-                	print("Your Username");
+                    $query =   "SELECT username 
+                                FROM users
+                                NATURAL JOIN profiles";
 
-                	$con = new PDO("mysql:host=localhost;dbname=chi", "root", "root");
-                	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    $data = $con->query($query);
+                    $data->setFetchMode(PDO::FETCH_CLASS, "Lostname");
 
-                	$query = "SELECT username FROM users
-                			  NATURAL JOIN profiles WHERE profiles.firstName = :firstName AND profiles.lastName = :lastName";
+                    print "    <table border='1'>\n";
 
-                	$ps = $con->prepare($query);
+                    $result = $con->query($query);
+                    $row = $result->fetch(PDO::FETCH_ASSOC);
 
-                	$ps->bindParam(':firstName', $firstName);
-                	$ps->bindParam(':lastName', $lastName);
-                	$ps->execute();
-                	$data = $ps->fetchAll(PDO::FETCH_ASSOC);
+                    print "            <tr>\n";
+                    foreach ($row as $field => $value) {
+                            print "                <th>$field</th>\n";
+                    }
+                    print "            </tr>\n";
 
-                	if(count($data) > 0){
-                		constructTable($data);
-                	}
-                	else{
-                		print("<h3>No username linked to that name</h3>");
-                	}
+        
+                    if ((strlen($firstName) > 0) && (strlen($lastName) > 0)){
+
+                        $query =   "SELECT username 
+                                    FROM users
+                                    NATURAL JOIN profiles 
+                                    WHERE profiles.firstName = :firstName 
+                                    AND profiles.lastName = :lastName";
+
+                        $ps = $con->prepare($query);
+                        $ps->bindParam(':firstName', $firstName);
+                        $ps->bindParam(':lastName', $lastName);
+                    }
+                    else{
+                        $ps = $con->prepare($query);
+                    }
+
+                    $ps->execute();
+                    $ps->setFetchMode(PDO::FETCH_CLASS, "Lostname");
+
+                    while($lost = $ps->fetch()){
+                        print "        <tr>\n";
+                        createTableRow($lost);
+                        print "        </tr>\n";
+                    }
+                    print "    </table>\n";
                 }
-                catch(PDOException $ex){
-                	echo 'ERROR: '.$ex->getMessage();
-                }
-                catch(Exception $ex){
-                	echo 'ERROR: '.$ex->getMessage();
-                }
+                catch(PDOException $ex) {
+                    echo 'ERROR: '.$ex->getMessage();
+                } 
+
+                //   function constructTable($data){
+                //     print("<table border='1'>\n");
+
+                //     // Constructs table header
+                //     $doHeader = true;
+                //     foreach ($data as $row) {
+                //         if($doHeader){
+                //             print("<tr>");
+                //             foreach ($row as $name => $value) {
+                //                 print("<th>$name</th>\n");
+                //             }
+                //             print("</tr>\n");
+                //             $doHeader = false;
+                //         }
+                //         // Constructs table row
+                //         print("<tr>\n");
+                //         foreach ($row as $name => $value) {
+                //             print("<td>$value</td>\n");
+                //         }
+                //         print("</tr>\n"); 
+                //     }
+                //     print("</table>\n");
+                // }
+
+                // $firstName = filter_input(INPUT_POST, "firstName");
+                // $lastName = filter_input(INPUT_POST, "lastName");
+
+                // try{
+                // 	if(empty($firstName) || empty($lastName)){
+                // 		throw new Exception("Missing first or last name");
+                // 	}
+
+                // 	print("Your Username");
+
+                // 	$con = new PDO("mysql:host=localhost;dbname=chi", "root", "root");
+                // 	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                // 	$query = "SELECT username FROM users
+                // 			  NATURAL JOIN profiles WHERE profiles.firstName = :firstName AND profiles.lastName = :lastName";
+
+                // 	$ps = $con->prepare($query);
+
+                // 	$ps->bindParam(':firstName', $firstName);
+                // 	$ps->bindParam(':lastName', $lastName);
+                // 	$ps->execute();
+                // 	$data = $ps->fetchAll(PDO::FETCH_ASSOC);
+
+                // 	if(count($data) > 0){
+                // 		constructTable($data);
+                // 	}
+                // 	else{
+                // 		print("<h3>No username linked to that name</h3>");
+                // 	}
+                // }
+                // catch(PDOException $ex){
+                // 	echo 'ERROR: '.$ex->getMessage();
+                // }
+                // catch(Exception $ex){
+                // 	echo 'ERROR: '.$ex->getMessage();
+                // }
                 ?>
             </main>
             <footer>
