070570ff2acf19deec8cfbb6990e72a5d48d4233
David Delgado <alohakealii@gmail.com>
2015-11-22 12:44:23 -0800

    initial google map commit

--- project/js/spartalunch.js
+++ project/js/spartalunch.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
-
-	$.get('../php/session_check.php', function(data) {
+	$.get('php/session_check.php', function(data) {
 		if (data.indexOf("active") != -1) {
-			$("nav").load("../parts/userheader.php");
+			$("nav").load("parts/userheader.php");
     }
 		else {
-			$("nav").load("../parts/anonheader.html");
+			$("nav").load("parts/anonheader.html");
     }
 	});
+
+  initMap();
 });
 
 function register() {
@@ -115,4 +116,83 @@ function match() {
 
 
     }
+}
+
+// function initMap() {
+//   // Create a map object and specify the DOM element for display.
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     center: {lat: 37.334993, lng: -121.881168},
+//     zoom: 12
+//   });
+// }
+
+function initMap() {
+  // run init the first time the modal opens
+  var created = false;
+  $('#map-modal').on('shown.bs.modal', function() {
+    if (!created) {
+      initAutocomplete();
+      created = true;
+    }
+  });
+}
+
+function initAutocomplete() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 37.334993, lng: -121.881168},
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    // map.fitBounds(bounds);
+  });
 }
