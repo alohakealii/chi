34c305cd5f639b5b794d26ae79e2ca4670b9c4ef
hansenwu <hansen.wu.85@gmail.com>
2015-11-30 21:51:38 -0800

    redirects anon user to 'plz login' page

---
+++ project/plzlogin.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="../../favicon.ico">
+
+  <title>spartalunch - Please Log In</title>
+
+  <link href="css/flatly.bootstrap.min.css" rel="stylesheet">
+  <link href="css/masonry.css" rel="stylesheet" type="text/css" >
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+    </head>
+
+    <body>
+
+      <div class="container">
+
+        <nav class="navbar navbar-default">
+        </nav>
+
+        <!-- Main component for a primary marketing message or call to action -->
+        <div class="jumbotron">
+          <h1>Oops!</h1>
+          <p>That page isn't accessible. You'll first need to <a href="login.php">log in</a> to spartalunch.</p>
+          <p>Don't have an account? You can <a href="register.php">register here</a>.</p>
+        </div>
+
+      </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/spartalunch.js"></script>
+    <!-- Session Check
+    <script src="js/sessionCheck.js"></script>
+    <script>window.onload=checkSession();</script> -->
+    <script src="js/home.js"></script>
+    <script src="js/imagesloaded.pkgd.js"></script>
+    <script src="js/masonry.pkgd.min.js"></script>
+  </body>
+  </html>
