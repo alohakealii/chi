9cce509fb9be78db562970e0f2a817660d80ee5b
quibs <quibie7@gmail.com>
2015-12-01 12:35:12 -0800

    maps
    maps are in, click restaurant pictures to open modal

--- project/home.php
+++ project/home.php
@@ -16,45 +16,53 @@
   <link href="css/flatly.bootstrap.min.css" rel="stylesheet">
   <link href="css/spartalunch.css" rel="stylesheet">
   <link href="css/masonry.css" rel="stylesheet" type="text/css" >
-  
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+</head>
 
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-      <![endif]-->
-    </head>
+<body>
+  <div class="container">
+    <nav class="navbar navbar-default">
+    </nav>
 
-    <body>
+    <div class="jumbotron resize-center">
+      <h1>Welcome <?php echo $_SESSION["firstName"]?>!</h1>
+      <p>First, set the times that you are <a href="availability.php">available</a> or check below for some food suggestions!</p>
+    </div>
 
-      <div class="container">
-
-        <nav class="navbar navbar-default">
-        </nav>
-
-        <!-- Main component for a primary marketing message or call to action -->
-        <div class="jumbotron resize-center">
-          <h1>Welcome <?php echo $_SESSION["firstName"]?>!</h1>
-          <p>First, set the times that you are <a href="availability.php">available</a> or check below for some food suggestions!</p>
+    <!-- Google Maps Modal -->
+    <div id="map-modal" class="modal fade" role="dialog">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <h4 class="modal-title">San Jose State University</h4>
+          </div>
+          <div class="modal-body">
+            <input id="pac-input" class="controls" type="text" placeholder="Search Box"/>
+            <div id="map"></div>
+          </div>
+          <!-- <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div> -->
         </div>
+      </div>
+    </div>
 
-        <div class="grid" id="grid">
-          <div class="grid-sizer"></div>
-        </div>
+    <div class="grid" id="grid">
+      <div class="grid-sizer"></div>
+    </div>
 
-      </div> <!-- /container -->
+  </div> <!-- /container -->
 
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places"></script>
     <script src="js/spartalunch.js"></script>
-    <!-- Session Check
-    <script src="js/sessionCheck.js"></script>
-    <script>window.onload=checkSession();</script> -->
     <script src="js/home.js"></script>
+    <!-- masonry library -->
     <script src="js/imagesloaded.pkgd.js"></script>
     <script src="js/masonry.pkgd.min.js"></script>
   </body>
--- project/js/home.js
+++ project/js/home.js
@@ -25,12 +25,16 @@ function retrieveLocationImages() {
   var outputArr = [];
   var images = JSON.parse(response);
   for (i = 0; i < images.length; i++) {
-    var html = '<div class="grid-item"><img src="' + images[i]["imgPath"] + '"/></div>';
+    var html = '<div class="grid-item"><img data-toggle="modal" data-target="#map-modal" onclick="setMapSearch(&quot;' + images[i]["name"] + '&quot;)" src="' + images[i]["imgPath"] + '"/></div>';
     outputArr [i] = html;
   }
   shuffle(outputArr);
   for (i = 0; i < outputArr.length; i++) {
     $('#grid').append(outputArr[i]);
-  }
-  
+  } 
+}
+
+function setMapSearch(name) {
+  //$('#pac-input').html(name);
+  document.getElementById('pac-input').value = name;
 }
